fb55119f49722c70d458d14f60d98b35bcc49e6e
Rayandra Yala Pratama <rayandra.pratama@gmail.com>
2023-04-08 20:05:43 +0700

    spesialiscontroller tambah SP.OT

--- app/Http/Controllers/API/SpesialisController.php
+++ app/Http/Controllers/API/SpesialisController.php
@@ -17,7 +17,7 @@ class SpesialisController extends Controller
      */
     public function index()
     {
-        $arrspesialis=array('Sp.A','Sp.An','Sp.B','Sp.BM','Sp.BP','Sp.BS','Sp.DV','Sp.JP','Sp.KF','Sp.KJ','Sp.KK','Sp.M','Sp.OG','Sp.OT','Sp.P','Sp.PA','Sp.PD','Sp.PK','Sp.Pr','Sp.Ra','Sp.S','Sp.TH','Sp.U','SpRad','TKV');
+        $arrspesialis=array('Sp.A','Sp.An','Sp.B','Sp.BM','Sp.BP','Sp.BS','Sp.OT','Sp.DV','Sp.JP','Sp.KF','Sp.KJ','Sp.KK','Sp.M','Sp.OG','Sp.OT','Sp.P','Sp.PA','Sp.PD','Sp.PK','Sp.Pr','Sp.Ra','Sp.S','Sp.TH','Sp.U','SpRad','TKV');
         $data = Spesialis::whereIn('kd_sps', $arrspesialis)->get();
         // $data = Spesialis::all();
         if ($data) {
